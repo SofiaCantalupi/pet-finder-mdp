c2059eb4711f756e8739855d176efea813ddb933
SofiaCantalupi <sofiacantalupi@gmail.com>
2025-05-29 14:34:48 -0300

    primer commit

---
+++ pet-finder-mdp/src/main/java/com/UTN/pet_finder_mdp/PetFinderMdpApplication.java
@@ -0,0 +1,13 @@
+package com.UTN.pet_finder_mdp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetFinderMdpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetFinderMdpApplication.class, args);
+	}
+
+}
---
+++ pet-finder-mdp/src/test/java/com/UTN/pet_finder_mdp/PetFinderMdpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UTN.pet_finder_mdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFinderMdpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
